55aceba779e861d4dfc47e5f295011461288a7e6
Gleb Antipenko <antipenko.g.d@gmail.com>
2021-03-08 18:41:44 +0300

    Update theme

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,32 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+  ".git/config": "350594e0ffc37e453dd0a82fbfe17197",
+".git/description": "a0a7c3fff21f2aea3cfa1d0316dd816c",
+".git/HEAD": "4cf2d64e44205fe628ddd534e1151b58",
+".git/hooks/applypatch-msg.sample": "ce562e08d8098926a3862fc6e7905199",
+".git/hooks/commit-msg.sample": "579a3c1e12a1e74a98169175fb913012",
+".git/hooks/fsmonitor-watchman.sample": "ecbb0cb5ffb7d773cd5b2407b210cc3b",
+".git/hooks/post-update.sample": "2b7ea5cee3c49ff53d41e00785eb974c",
+".git/hooks/pre-applypatch.sample": "054f9ffb8bfe04a599751cc757226dda",
+".git/hooks/pre-commit.sample": "e4db8c12ee125a8a085907b757359ef0",
+".git/hooks/pre-merge-commit.sample": "39cb268e2a85d436b9eb6f47614c3cbc",
+".git/hooks/pre-push.sample": "3c5989301dd4b949dfa1f43738a22819",
+".git/hooks/pre-rebase.sample": "56e45f2bcbc8226d2b4200f7c46371bf",
+".git/hooks/pre-receive.sample": "2ad18ec82c20af7b5926ed9cea6aeedd",
+".git/hooks/prepare-commit-msg.sample": "2b5c047bdb474555e1787db32b2d2fc5",
+".git/hooks/update.sample": "517f14b9239689dff8bda3022ebd9004",
+".git/index": "7aba92ddf8b5248d8baf45b3c8b52208",
+".git/info/exclude": "036208b4a1ab4a235d75c181e685e5a3",
+".git/logs/HEAD": "0a1ccc77ac2aab733701de9e673c1fff",
+".git/logs/refs/heads/master": "0a1ccc77ac2aab733701de9e673c1fff",
+".git/logs/refs/remotes/origin/HEAD": "0a1ccc77ac2aab733701de9e673c1fff",
+".git/objects/pack/pack-98d2330ef987c260118c85bc6b088ed3dff0bbf4.idx": "0e0b3ee68bae3bfc965732062e750fdf",
+".git/objects/pack/pack-98d2330ef987c260118c85bc6b088ed3dff0bbf4.pack": "c330892b2670db5c199916d93b28c4aa",
+".git/packed-refs": "7a4588d8a4c45e2398f8bf4d376376fa",
+".git/refs/heads/master": "3ad5173b40326e043bf796d9a2f86af6",
+".git/refs/remotes/origin/HEAD": "73a00957034783b7b5c8294c54cd3e12",
+"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
 "assets/NOTICES": "a8d1d130b9909728d724354dc06becc1",
@@ -11,11 +36,12 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "1e3e1f921b35163261b0e2f5db45f1c3",
-"/": "1e3e1f921b35163261b0e2f5db45f1c3",
-"main.dart.js": "8a29ae24f17cde64c119c935f1edad38",
+"index.html": "f89d451e64c9055915603b5aab20f3a8",
+"/": "f89d451e64c9055915603b5aab20f3a8",
+"main.dart.js": "50cfd738164320e2b0e9dc1edd2dc246",
 "manifest.json": "0c18e86e927ae28524a05658d7c07aaa",
-"version.json": "59194350a298e893e1aa7570c133abf4"
+"version.json": "59194350a298e893e1aa7570c133abf4",
+"_config.yml": "932865e374757d33e321eeb2d5e88b6b"
 };
 
 // The application shell files that are downloaded before a service worker can
